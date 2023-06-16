25f13b2df8ab41378442c498f26620ecb781778a
jackson Savoldi <goldenhorsescountry@gmail.com>
2023-06-16 17:59:02 -0300

    up

---
+++ casino-app/precache-manifest.f374f84e2cec68c7f8be8e76ff866407.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34916de09eacb0ef0ce662e79da24f5",
+    "url": "/casino-app/index.html"
+  },
+  {
+    "revision": "255111ffb78b04c586ea",
+    "url": "/casino-app/static/css/main.eedc7eae.chunk.css"
+  },
+  {
+    "revision": "2e59125ac70cad8fd4d6",
+    "url": "/casino-app/static/js/2.cdb40f18.chunk.js"
+  },
+  {
+    "revision": "255111ffb78b04c586ea",
+    "url": "/casino-app/static/js/main.02602035.chunk.js"
+  },
+  {
+    "revision": "861834d73bfa21f73739",
+    "url": "/casino-app/static/js/runtime-main.321989f9.js"
+  },
+  {
+    "revision": "84e1352a120482d9b1548a91ef71a652",
+    "url": "/casino-app/static/media/bg.84e1352a.png"
+  },
+  {
+    "revision": "50a6406b14318918118039d917c4504d",
+    "url": "/casino-app/static/media/figure.50a6406b.png"
+  },
+  {
+    "revision": "64c89343a7f0d3cf05cad440a4a28f47",
+    "url": "/casino-app/static/media/icon.64c89343.svg"
+  },
+  {
+    "revision": "992a209e77d678466bd43910ef6b0afc",
+    "url": "/casino-app/static/media/logo.992a209e.svg"
+  }
+]);
